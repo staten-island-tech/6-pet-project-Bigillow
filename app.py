7328decdbfb8230abdc2bb701a13b0bb982f521c
Bigillow <tsunheil@nycstudents.net>
2026-03-05 09:26:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-class Hero:
-    def __init__(self, name, money, inventory):
+# class Hero:
+#     def __init__(self, name, money, inventory):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
+
+#     def buy(self, item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+
+# Jimmy = Hero("Jimmy", 1000000, [{"Title": "Socks", "Warmth": 5}])
+
+# Jimmy.buy({"Title": "Pants", "Warmth": 10})
+
+# print(Jimmy.__dict__)
+
+class Pet:
+    def __init__(self, name, hppiness):
         self.name = name
-        self.money = money
-        self.inventory = inventory
+        self.__hppiness = hppiness
+    def play(self):
+        print(f"{self.name} is now playing fetch.")
+        self.__hppiness += 10
+    def show_status(self):
+        print(f"{self.name}’s happiness is now {self.__hppiness}")
 
-    def buy(self, item):
-        self.inventory.append(item)
-        print(self.inventory)
+Dawg = Pet("Dawg", 0)
 
-Jillian = Hero("Jillian", 150, ["Potion"])
 
-Jillian.buy({"title": "Sword", "atk": 34})
-print(Jillian.__dict__)
+    
